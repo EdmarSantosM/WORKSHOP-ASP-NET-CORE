0c9821d01831110fb0eed86898728b53ce21ff30
EdmarSantosM <santosem4@hotmail.com>
2023-05-12 14:11:44 -0300

    Gerando a primeira migração
    primeira Migração
    dotnet ef migrations add InitialCreate
    
    Comando que cria  a tabela
    dotnet ef database update

---
+++ src/FilmesCRUDRazor/Migrations/20230512165551_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmesCRUDRazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filme",
+                columns: table => new
+                {
+                    FilmeId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: true),
+                    DataLancamento = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Genero = table.Column<string>(type: "TEXT", nullable: true),
+                    Preco = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filme", x => x.FilmeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filme");
+        }
+    }
+}
