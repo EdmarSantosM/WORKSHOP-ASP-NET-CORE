f9b187ce602bff12c8d1d49e857265126ea485ec
EdmarSantosM <santosem4@hotmail.com>
2023-05-13 22:49:31 -0300

    Data Annotations
    Required =  torna  campo obrigatório
    
    Display(Name) formata nome do (Título)
    
    DisplayFormat(dataFormatString = formata a visualização do  DateTime
    
    DataType.Currency = Formata a moeda corrente  a ser exibida.
    
    Column TypeName = Formata casas decimais

--- src/FilmesCRUDRazor/Models/Filme.cs
+++ src/FilmesCRUDRazor/Models/Filme.cs
@@ -7,14 +7,29 @@ namespace FilmesCRUDRazor.Models
 {
     public class Filme
     {
+        
         public int FilmeId { get; set; }
 
+        [Display(Name ="Título do Filme")]
+        [Required]
         public string? Titulo { get; set; }
 
+
+
+        [Display(Name ="Data de Lançamento")]
+        [DisplayFormat(DataFormatString ="{0: dd/MM/yyyy}",ApplyFormatInEditMode = true)]
         public DateTime DataLancamento { get; set; }
 
+
+
+        [Display(Name ="Gênero")]
+        [Required]
         public string? Genero { get; set; }
 
+
+        [Display(Name ="Preço")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName ="decimal(18,2)")]
         public decimal Preco { get; set; }
     }
 }
