831ed1eb7449cf3f3178f7482a7ad94e6006f0ee
EdmarSantosM <santosem4@hotmail.com>
2023-05-14 22:59:10 -0300

    Update
    ..

--- src/FilmesCRUDRazor/Pages/Filmes/Index.cshtml.cs
+++ src/FilmesCRUDRazor/Pages/Filmes/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using FilmesCRUDRazor.Models;
 
+using Microsoft.AspNetCore.Mvc.Rendering; // Importação - (Uso de Filtros)
+
 namespace FilmesCRUDRazor.Pages.Filmes
 {
     public class IndexModel : PageModel
@@ -18,10 +20,46 @@ namespace FilmesCRUDRazor.Pages.Filmes
             _context = context;
         }
 
-        public IList<Filme> Filme { get;set; } = default!;
+        public IList<Filme> Filme { get;set; }// = default!;
 
-        public async Task OnGetAsync( string buscaPorGeneroNome)
+        public SelectList Generos;
+
+        public string FilmePorGenero{get; set;}
+
+        public async Task OnGetAsync( string buscaPorGeneroNome, string filmePorGenero)
         {
+
+            #region  Input
+            //Uso do Linq e  expressão lambda
+
+             /* select genero from filmes where filme.genero == genero */
+
+            IQueryable<string> queryGenero = from f in _context.Filme
+                                                orderby f.Genero
+                                                select f.Genero;
+            
+
+
+            var filmes =  from f in _context.Filme
+                            select f;
+
+            if(!String.IsNullOrEmpty(buscaPorGeneroNome))
+            {
+                filmes = filmes.Where(b => b.Titulo.Contains(buscaPorGeneroNome));
+            }
+            #endregion
+
+
+            #region  // select
+                if(!String.IsNullOrEmpty(filmePorGenero))
+                {
+                    filmes = filmes.Where(b => b.Genero == filmePorGenero);
+                }
+
+            #endregion
+
+
+
             if (_context.Filme != null)
             {
                 Filme = await _context.Filme.ToListAsync();
